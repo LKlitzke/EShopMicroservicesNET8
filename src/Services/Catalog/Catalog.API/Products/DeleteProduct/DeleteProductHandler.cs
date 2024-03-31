9a70cf501852a914c428454dc749e28192cb8d0e
Lucas Klitzke <lucas_eduardoksilva@hotmail.com>
2024-03-31 17:10:05 -0300

    Adding Catalog endpoints

--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -1,6 +1,26 @@
-﻿namespace Catalog.API.Products.DeleteProduct
+﻿namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductCommand(Guid Id)
+    : ICommand<DeleteProductResult>;
+
+public record DeleteProductResult(bool IsSuccess);
+
+internal class DeleteProductCommandHandler
+    (IDocumentSession session, ILogger<DeleteProductCommandHandler> logger)
+    : ICommandHandler<DeleteProductCommand, DeleteProductResult>
 {
-    public class DeleteProductHandler
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
     {
+        logger.LogInformation("DeleteProductCommandHandler.Handle called with {@Command}", command);
+
+        var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+
+        if (product == null)
+            throw new ProductNotFoundException(command.Id);
+
+        session.Delete<Product>(product.Id);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new DeleteProductResult(true);
     }
 }
--- src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -1,6 +1,21 @@
-﻿namespace Catalog.API.Products.GetProductByCategory
+﻿namespace Catalog.API.Products.GetProductByCategory;
+
+public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+
+public record GetProductByCategoryResult(IEnumerable<Product> Products);
+
+internal class GetProductByCategoryQueryHandler
+     (IDocumentSession session, ILogger<GetProductByCategoryQuery> logger)
+    : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
 {
-    public class GetProductByCategoryHandler
+    public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
     {
+        logger.LogInformation("GetProductByCategoryQueryHandler.Handle called with {@Query}", query);
+
+        var products = await session.Query<Product>()
+            .Where(p => p.Category.Contains(query.Category))
+            .ToListAsync(cancellationToken);
+
+        return new GetProductByCategoryResult(products);
     }
 }
