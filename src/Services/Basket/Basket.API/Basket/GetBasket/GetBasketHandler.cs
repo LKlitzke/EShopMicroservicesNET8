2fb8b2fc4ab4a34fd62d20949b8886170278b653
Lucas Klitzke <lucas_eduardoksilva@hotmail.com>
2024-04-04 23:48:45 -0300

    Adding Basket Endpoints

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Basket.API.Basket.GetBasket;
+
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+public record GetBasketResult(ShoppingCart Cart);
+
+public class GetBasketQueryHandler(IBasketRepository basketRepository) : IQueryHandler<GetBasketQuery, GetBasketResult>
+{
+    private readonly IBasketRepository _basketRepository = basketRepository;
+
+    public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+    {
+        var basket = await _basketRepository.GetBasket(query.UserName, cancellationToken);
+
+        return new GetBasketResult(basket);
+    }
+}
