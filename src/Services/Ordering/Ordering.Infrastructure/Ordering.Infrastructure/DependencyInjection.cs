997f32baf880acd02643741edfca5dc7421e8acb
Lucas Klitzke <lucas_eduardoksilva@hotmail.com>
2024-04-06 15:17:42 -0300

    Creating Ordering.Infrastructure module

---
+++ src/Services/Ordering/Ordering.Infrastructure/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Data;
+
+namespace Ordering.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices (this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Database");
+
+        // Add services to the container.
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+        services.AddDbContext<AppDbContext>((sp, opts) =>
+        {
+            opts.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+            opts.UseSqlServer(connectionString);
+        });
+
+        services.AddScoped<IAppDbContext, AppDbContext>();
+
+        return services;
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Ordering.Infrastructure/GlobalUsing.cs
@@ -0,0 +1,8 @@
+﻿global using Ordering.Infrastructure.Data;
+global using Ordering.Infrastructure.Data.Interceptors;
+
+global using Ordering.Domain.Abstractions;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
+
+global using Microsoft.EntityFrameworkCore;
