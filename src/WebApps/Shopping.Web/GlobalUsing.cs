8658337a3ccb5fadf7cd5bd95c48f50be34e5398
Lucas Klitzke <lucas_eduardoksilva@hotmail.com>
2024-04-13 11:30:58 -0300

    Adding Shopping.Web basic project

---
+++ src/WebApps/Shopping.Web/GlobalUsing.cs
@@ -0,0 +1,10 @@
+﻿global using Shopping.Web.Models.Catalog;
+global using Shopping.Web.Models.Basket;
+global using Shopping.Web.Models.Ordering;
+
+global using Refit;
+
+global using Shopping.Web.Services;
+
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.RazorPages;
